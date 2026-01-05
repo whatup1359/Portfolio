908bb825de0e500edb3a61f704aafa66c22bde5a
whatup1359 <wutza_1779@hotmail.com>
2026-01-05 17:26:49 +0700

    fix responsive

--- app/components/education/page.tsx
+++ app/components/education/page.tsx
@@ -24,6 +24,7 @@ const Education = () => {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.1 }}
+          className="flex flex-col items-center justify-center"
         >
           <div className="hidden xl:flex lg:flex md:flex items-center justify-center">
             <div className="w-[700px] px-25 xl:px-0 lg:px-20 md:px-30 sm:px-40">
@@ -34,15 +35,19 @@ const Education = () => {
             </div>
           </div>
 
-          <div className="flex items-center justify-center">
-            <div className="flex flex-col mt-10 xl:mt-0 lg:mt-0 md:mt-0">
-              <span className="xl:hidden lg:hidden md:hidden mb-2">2020-2023</span>
-              <div className="h-[1.5px] w-[300px] xl:w-[800px] lg:w-[650px] md:w-[550px] sm:w-[400px] bg-linear-to-r from-neutral-900 via-neutral-400 to-neutral-900 rounded-full" />
-            </div>
+          <div className="hidden items-center justify-center xl:flex lg:flex md:flex mt-10 xl:mt-0 lg:mt-0 md:mt-0">
+            <div className="h-[1.5px] w-[300px] xl:w-[800px] lg:w-[650px] md:w-[550px] sm:w-[400px] bg-linear-to-r from-neutral-900 via-neutral-400 to-neutral-900 rounded-full" />
           </div>
 
           <div className="flex items-center justify-center">
             <div className="flex justify-between px-6 flex-col xl:flex-row lg:flex-row md:flex-row mt-5 xl:space-x-80 lg:space-x-60 md:space-x-20 space-y-10">
+              <div className="flex flex-col xl:hidden lg:hidden md:hidden">
+                <span className="xl:hidden lg:hidden md:hidden mb-2">
+                  2016-2019
+                </span>
+                <div className="h-[1.5px] xl:w-[800px] lg:w-[700px] md:w-[600px] sm:w-[400px] bg-linear-to-r from-neutral-900 via-neutral-400 to-neutral-900 rounded-full" />
+              </div>
+
               <div className="space-y-1">
                 <ul className="text-3xl pb-4">High School</ul>
                 <li>Suankularb Wittayalai Thonburi</li>
@@ -53,7 +58,7 @@ const Education = () => {
                 <span className="xl:hidden lg:hidden md:hidden mb-2">
                   2020-2023
                 </span>
-                <div className="h-[1.5px] w-[300px] xl:w-[800px] lg:w-[700px] md:w-[600px] sm:w-[400px] bg-linear-to-r from-neutral-900 via-neutral-400 to-neutral-900 rounded-full" />
+                <div className="h-[1.5px] xl:w-[800px] lg:w-[700px] md:w-[600px] sm:w-[400px] bg-linear-to-r from-neutral-900 via-neutral-400 to-neutral-900 rounded-full" />
               </div>
 
               <div className="space-y-1">
